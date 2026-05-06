d41f6381779d8b825beda645d6283945ab56babb
Spiros Skarmoutsos <skarmspiros@gmail.com>
2026-05-06 20:53:48 +0000

    completed the outputs for 4.2

--- advcomparch-ex2-helpcode/run_benchmarks.sh
+++ advcomparch-ex2-helpcode/run_benchmarks.sh
@@ -10,22 +10,15 @@ outDir="/home/ssm-user/advcomparch/advcomparch-ex2-helpcode/outputs/L2_cache"
 inputBase="/home/ssm-user/advcomparch/advcomparch-ex2-helpcode/spec_benchmarks"
 
 ## Triples of <cache_size>_<associativity>_<block_size>
-CONFS="256_4_64 256_4_128 256_4_256"
-#  \
-# 512_4_64 512_4_128 512_4_256 \
-# 512_8_64 512_8_128 512_8_256 \
-# 512_16_64 512_16_128 512_16_256 \
-# 1024_8_64 1024_8_128 1024_8_256 \
-# 1024_16_64 1024_16_128 1024_16_256 \
-# 2048_16_64 2048_16_128 2048_16_256"
+CONFS="2048_16_64 2048_16_128 2048_16_256"
 
 L1size=32
 L1assoc=4
 L1bsize=32
 
 # Loop over every subfolder in the input base directory
-# for folder in "$inputBase"/*; do
-folder="$inputBase/$1"
+for folder in "$inputBase"/*; do
+#folder="$inputBase/$1"
     if [ -d "$folder" ]; then
         BENCH=$(basename "$folder")
         (
@@ -71,7 +64,7 @@ folder="$inputBase/$1"
 	    wait
         ) &
     fi
-# done
+ done
 
 wait
 echo "All benchmarks done."
